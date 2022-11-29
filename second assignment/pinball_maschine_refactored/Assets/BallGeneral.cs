b1723cae39755b1fa655e1acf6305b1246fff4b2
Moritz <morsen@live.de>
2022-11-29 11:00:17 +0100

    FINAL ONLY CHANGE SOUND

--- second assignment/pinball_maschine_refactored/Assets/BallGeneral.cs
+++ second assignment/pinball_maschine_refactored/Assets/BallGeneral.cs
@@ -7,7 +7,6 @@ public class BallGeneral : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -16,6 +15,7 @@ public class BallGeneral : MonoBehaviour
         if ((GetComponent<Rigidbody>() != null) && GetComponent<Rigidbody>().IsSleeping())
         {
             GetComponent<Rigidbody>().WakeUp();
+
         }
     }
 }
